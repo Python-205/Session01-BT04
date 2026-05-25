68ab1f06751cc4e13054a03c71abe4c53c2f371c
Nguyenvan Hoan <nguyenhoan18042007@gmail.com>
2026-05-25 10:01:46 +0700

    first commit

---
+++ lession04.py
@@ -0,0 +1,9 @@
+print("-- Kết Quả Chuẩn Hóa Dữ Liệu--")
+code_bn = input(" Mã bệnh nhân:")
+temperature = float(input("Nhiệt độ cơ thể:"))
+heart_rate = int(input("Nhịp tim:"))
+
+print('kẾT QUẢ')
+print(f"Mã Bệnh Nhân:{code_bn}")
+print(f"Nhiệt Độ Cơ Thể: {temperature}")
+print(f"Nhip Tim:{heart_rate}")
