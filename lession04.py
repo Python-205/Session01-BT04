1fd5b3766be93ad8f4c240b515d0916fb4d829fb
nguyenhoan18042007-create <nguyenhoan18042007@gmail.com>
2026-05-26 17:39:38 +0700

    Add files via upload

--- lession04.py
+++ lession04.py
@@ -7,3 +7,17 @@ print('kẾT QUẢ')
 print(f"Mã Bệnh Nhân:{code_bn}")
 print(f"Nhiệt Độ Cơ Thể: {temperature}")
 print(f"Nhip Tim:{heart_rate}")
+
+
+# Input:
+# - Mã bệnh nhân      -> kiểu chuỗi (str)
+# - Nhiệt độ cơ thể   -> cần lưu kiểu số thực (float)
+# - Nhịp tim          -> cần lưu kiểu số nguyên (int)
+
+# Vì dữ liệu nhập từ input() luôn là string
+# nên cần ép kiểu dữ liệu bằng:
+# - float() cho nhiệt độ
+# - int() cho nhịp tim
+# Sử dụng Type Casting:
+# float()  -> chuyển chuỗi sang số thực
+# int()    -> chuyển chuỗi sang số nguyên
